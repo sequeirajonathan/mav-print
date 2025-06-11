e59e442d10dde70ac5e2fd1aa01254898ca3bd39
sequeirajonathan <sequeira.s.jonathan@gmail.com>
2025-06-11 16:55:44 -0500

    Enhance Electron integration by adding sign-out listener and improving printer selection logic

--- src/preload.ts
+++ src/preload.ts
@@ -1,4 +1,5 @@
 import { contextBridge, ipcRenderer } from 'electron';
+import type { IpcRendererEvent } from 'electron';
 import { PrintCommand, PrintResponse, PrintJobUpdate } from './types';
 
 // Expose protected methods that allow the renderer process to use
@@ -6,10 +7,22 @@ import { PrintCommand, PrintResponse, PrintJobUpdate } from './types';
 contextBridge.exposeInMainWorld('electron', {
   agentId: process.env.AGENT_ID || 'agent-unknown',
   sendPrintCommand: (command: PrintCommand) => ipcRenderer.send('print-command', command),
-  onPrintResponse: (callback: (event: Electron.IpcRendererEvent, response: PrintResponse) => void) => 
+  onPrintResponse: (callback: (event: IpcRendererEvent, response: PrintResponse) => void) => 
     ipcRenderer.on('print-response', callback),
-  onPrintJobUpdate: (callback: (event: Electron.IpcRendererEvent, payload: PrintJobUpdate) => void) => 
+  onPrintJobUpdate: (callback: (event: IpcRendererEvent, payload: PrintJobUpdate) => void) => 
     ipcRenderer.on('print-job-update', callback),
+  onSignOut: (callback: () => void) => {
+    console.log('Registering electron-sign-out listener');
+    const wrappedCallback = () => {
+      console.log('Received electron-sign-out event');
+      callback();
+    };
+    ipcRenderer.on('electron-sign-out', wrappedCallback);
+    return () => {
+      console.log('Removing electron-sign-out listener');
+      ipcRenderer.removeListener('electron-sign-out', wrappedCallback);
+    };
+  },
   invoke: (channel: string, ...args: any[]) => ipcRenderer.invoke(channel, ...args),
   send: (channel: string, ...args: any[]) => ipcRenderer.send(channel, ...args),
   on: (channel: string, listener: (...args: any[]) => void) => ipcRenderer.on(channel, listener),
